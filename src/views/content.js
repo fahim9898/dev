fd58eb80e5c7fa21c957e9af7a4b673f1c93ab3d
Fahim Sanghariyat <sanghariyatfahim99@gmail.com>
2021-08-29 23:19:39 +0530

    feat: add junubi tunes page

--- src/views/content.js
+++ src/views/content.js
@@ -2,32 +2,26 @@ export default {
     projects:[
       {
         id: 1,
-        title: 'Alpha-Zero',
-        sort_description: ` Alpha zero is the game bot which plays
-              <span class="highlight"> Tic-Tac-Toe </span> game. A goal of the
-              bot is to beat humans in Tic-Tac-Toe.`,
-        website_link: 'https://github.com/',
-        github_link: `https://github.com/`,
+        title: 'Junubi Tunes',
+        sort_description: `<span class="highlight"> Junubi Tunes </span> is amazing music application. For listening top trending songs all around the world`,
+        website_link: 'https://sudosounds.net/',
+        github_link: ``,
         images: [
-          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Falpha-zero%2Falpha-zero-win.png?alt=media&token=41e6081b-c3a2-494a-9d70-37530e3cfb4e',
-          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Falpha-zero%2Falpha-zero-win.png?alt=media&token=41e6081b-c3a2-494a-9d70-37530e3cfb4e',
-          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Falpha-zero%2Falpha-zero-win.png?alt=media&token=41e6081b-c3a2-494a-9d70-37530e3cfb4e',
+          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Fjunubi%2Fjunubi1.png?alt=media&token=3bdd3e7d-5eda-4b4d-b031-b6742c1be903',
+          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Fjunubi%2Fjunubi2.png?alt=media&token=93b29f0c-bb90-490b-a2b6-33096ea03c1d',
+          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Fjunubi%2Fjunubi3.png?alt=media&token=31a97af2-f5e3-4f78-8c6d-2bc75b2d7ee0',
+          'https://firebasestorage.googleapis.com/v0/b/sonic-harbor-236704.appspot.com/o/Projects%2FWeb-dev%2Fjunubi%2Fjunubi4.png?alt=media&token=09637bf5-e342-4344-9be3-4362b82a4d06'
         ],
-        about_project: `There are many variations of passages of Lorem Ipsum available,
-                but the majority have suffered alteration in some form, by
-                injected humour, or randomised words which don't look even
-                slightly believable.<br><br> If you are going to use a passage of Lorem
-                Ipsum, you need to be sure there isn't anything embarrassing
-                hidden in the middle of text. All the Lorem Ipsum generators on
-                the Internet tend to repeat predefined chunks as necessary,
-                making this the first true generator on the Internet. It uses a
-                dictionary of over 200 Latin words,`,
+        about_project: `Junubi Tunes is music application for Web and Mobile users. In which have latest songs, Albums, Artist, Genres, Playlists and lots of features.
+        <br><br>
+        Junubi Tunes is music application for Web and Mobile users. In which have latest songs, Albums, Artist, Genres, Playlists and lots of features.`,
         tech_info: [
-            'Vue JS',
-            'Webpack',
-            'Babel'
+            'Laravel',
+            'SCSS',
+            'Bootrsrap',
+            'Jplayer'
         ],
-        resorce: `Blog: <a class="highlight" href="https://www.google.com">https://www.google.com</a>`,
+        resorce: `Link: <a target="_blank" class="highlight" href="https://sudosounds.net/">Weblink</a>`,
       }, 
       {
         id: 2,
